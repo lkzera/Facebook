8e3aa7d1862e5c97467fdd1a9beed1edbf7e2ae8
Lucas Lkzs <lucas1212.davi@hotmail.com>
2021-11-23 22:46:11 -0300

    Ajustes finais 2

--- listar_usuarios.php
+++ listar_usuarios.php
@@ -2,10 +2,12 @@
 include_once './Db/User/UserRepository.php';
 include_once './Models/Usuario.php';
 
-$nome = $_GET["nome"];
+$nome = isset($_GET["nome"]) ? $_GET["nome"] : null;
+$email = isset($_GET["email"]) ? $_GET["email"] : null;
+$login = isset($_GET["login"]) ? $_GET["login"] : null;
 
 $_userRepository = new UserRepository();
 
-$_resul = $_userRepository->SearchUserAndFriends("lucas",null,null);
+$_resul = $_userRepository->SearchUserAndFriends($nome,$login,$email);
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($_resul);
